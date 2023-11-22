5cea8b47bce3cf866454c84a19bc386ba1c84de0
Laura Martens <laura.d.martens@icloud.com>
2023-11-22 17:16:20 +0000

    Add metrics

---
+++ src/deconvatac/tl/metrics.py
@@ -0,0 +1,50 @@
+from anndata import AnnData
+import pandas as pd
+import numpy as np
+
+from typing import Optional, Union
+
+from scipy.spatial.distance import jensenshannon
+
+
+def rmse(
+        true: Union[pd.DataFrame, np.ndarray],
+        predicted: Union[pd.DataFrame, np.ndarray]
+        ) -> float:
+    """Compute RMSE on true and predicted cell type proportions.
+
+    Parameters
+    ----------
+    true
+        True cell type proportions.
+    predicted
+        Predicted cell type proportions.
+
+    Returns
+    -------
+    Root mean squared error.
+    """
+    rmse = np.sqrt(np.mean((true - predicted) ** 2))
+    return rmse
+
+def jsd(
+        true: Union[pd.DataFrame, np.ndarray], 
+        predicted: Union[pd.DataFrame, np.ndarray]
+        ) -> float:
+    """Compute Jensen-Shannon divergence on true and predicted cell type proportions.
+
+    Parameters
+    ----------
+    true
+        True cell type proportions.
+    predicted
+        Predicted cell type proportions.
+
+    Returns
+    -------
+    Jensen-Shannon divergence.
+    """
+    jsd = jensenshannon(true, predicted, axis=1, base=2)
+    return jsd.mean()
+
+
