fb728748dab5ee49d3c8c0193749d6b02f5be82d
Laura Martens <laura.d.martens@icloud.com>
2024-01-23 13:19:09 +0000

    Compute metrics

--- src/deconvatac/tl/__init__.py
+++ src/deconvatac/tl/__init__.py
@@ -1,8 +1,8 @@
 from .cell2location import cell2location
 from .tangram import tangram
 from .destvi import destvi
-#from .metrics import *
+from .metrics import jsd, rmse
 from .rctd import rctd
-#from .simulate import *
+from .simulate import *
 from .spatialdwls import spatialdwls
 #from .lsi import lsi
