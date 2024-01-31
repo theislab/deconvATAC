e011ee839d8cadf9cb4a2bb8906f4b7b6a9c551f
SarahOuologuem <sarah.ouologuem@gmx.de>
2024-01-31 15:41:34 +0000

    add destvi

--- src/deconvatac/tl/__init__.py
+++ src/deconvatac/tl/__init__.py
@@ -1,6 +1,6 @@
 from .cell2location import cell2location
 from .tangram import tangram
-#from .destvi import destvi
+from .destvi import destvi
 #from .metrics import *
 #from .rctd import rctd
 #from .simulate import *
