7d39b51af3201f2fb195d840e8f1be2e39c809c1
SarahOuologuem <sarah.ouologuem@gmx.de>
2025-03-18 15:57:38 +0100

    remove pl

--- src/deconvatac/__init__.py
+++ src/deconvatac/__init__.py
@@ -1,7 +1,15 @@
 from importlib.metadata import version
 
-from . import pl, pp, tl
 
-__all__ = ["pl", "pp", "tl"]
+from . import  pp, tl
+
+__all__ = ["pp", "tl"]
 
 __version__ = version("deconvATAC")
+
+
+#from . import pl, pp, tl
+
+#__all__ = ["pl", "pp", "tl"]
+
+#__version__ = version("deconvATAC")
